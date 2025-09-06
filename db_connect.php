91b02714b5f7ecf1a308014a785a84ea14855b6e
itzmefahim <hafiz_41230301887@nub.ac.bd>
2025-09-07 02:14:20 +0600

    Make compatible with xampp

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,14 @@
 <?php
 $host = "localhost";
-$user = "swift_user";
-$password = "swift_pass";
+$user = "root";
+$password = "";
 $database = "swift_book";
 
 $conn = new mysqli($host, $user, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "";
 }
 ?>
